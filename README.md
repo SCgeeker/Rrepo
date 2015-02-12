fa99f568fad58524f71493b77222b0e8aef9e353
SCgeeker <pmsp96@gmail.com>
2015-02-12 18:35:24 +0800

    "formatR" for display Chinese in Rmd

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Sau-Chin Chen's R scripts and projects
 
 20150209 Initiate
+20150212 add "formatR"
--- initial.R
+++ initial.R
@@ -1,1 +1,5 @@
 install.packages(c("rpart","chron","Hmisc","Design","Matrix","stringr","lme4","coda","e1071","zipfR","ape","languageR","multcomp","contrast","shiny"),repos="http://cran.csie.ntu.edu.tw/")
+# For display Chinese correctly
+install.packages("formatR", repos = "http://cran.rstudio.com")
+#' to install the development version, run
+#' install.packages('formatR', repos = 'http://rforge.net')
